5bb71543ccc6a2fc15b711264b61905bfbe3fd21
Jeremy Bierly <jbierly@figyrdigital.com>
2022-08-07 19:26:36 -0500

    adds pages

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,14 +1,15 @@
 import { Fragment } from 'react';
+import { Link } from '@remix-run/react';
 import { Popover, Transition } from '@headlessui/react';
 import { MenuIcon, XIcon } from '@heroicons/react/outline';
 import Logo from '../../app/images/logo.png';
 import LogoWhite from '../../app/images/logo-white.png';
 
 const navigation = [
-  { name: 'Services', href: '#' },
-  { name: 'About', href: '#' },
-  { name: 'Insights', href: '#' },
-  { name: 'Contact', href: '#' },
+  { name: 'Services', href: '/services' },
+  { name: 'About', href: '/about' },
+  { name: 'Insights', href: '/insights' },
+  { name: 'Contact', href: '/contact' },
 ];
 
 function Navbar() {
@@ -21,14 +22,14 @@ function Navbar() {
         >
           <div className="flex items-center flex-1">
             <div className="flex items-center justify-between w-full md:w-auto">
-              <a href="#">
+              <Link to="/">
                 <span className="sr-only">ClickCo</span>
                 <img
                   className="h-8 w-auto sm:h-10"
                   src={LogoWhite}
                   alt=""
                 />
-              </a>
+              </Link>
               <div className="-mr-2 flex items-center md:hidden">
                 <Popover.Button className="bg-clickco-blue rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-800 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
@@ -38,23 +39,23 @@ function Navbar() {
             </div>
             <div className="hidden font-kumbh-sans space-x-8 md:flex md:ml-10">
               {navigation.map((item) => (
-                <a
+                <Link
                   key={item.name}
-                  href={item.href}
+                  to={item.href}
                   className="text-base font-bold text-white hover:text-teal-200"
                 >
                   {item.name}
-                </a>
+                </Link>
               ))}
             </div>
           </div>
           <div className="hidden md:flex md:items-center md:space-x-6">
-            <a
-              href="#"
+            <Link
+              to="/consultation"
               className="inline-flex items-center px-4 py-2 border border-transparent text-base font-bold rounded-md text-white bg-teal-600 hover:bg-teal-700"
             >
               Free Consultation
-            </a>
+            </Link>
           </div>
         </nav>
       </div>
@@ -87,22 +88,22 @@ function Navbar() {
             <div className="pt-5 pb-6">
               <div className="px-2 space-y-1">
                 {navigation.map((item) => (
-                  <a
+                  <Link
                     key={item.name}
-                    href={item.href}
+                    to={item.href}
                     className="block px-3 py-2 rounded-md text-base font-medium text-clickco-blue hover:bg-gray-50"
                   >
                     {item.name}
-                  </a>
+                  </Link>
                 ))}
               </div>
               <div className="mt-6 px-5">
-                <a
-                  href="#"
+                <Link
+                  to="#"
                   className="block text-center w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 text-white font-medium hover:from-teal-600 hover:to-cyan-700"
                 >
                   Free Consultation
-                </a>
+                </Link>
               </div>
             </div>
           </div>
